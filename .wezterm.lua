8faf1f2d006e1a3fd85dca2ebea35af32babb6c5
abdomash <abdomash2010@gmail.com>
2025-01-21 09:05:08 -0600

    Fix audio bell in Windows

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,6 +5,7 @@ config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_padding = { left = 5, right = 5, top = 5, bottom = 0 }
 config.native_macos_fullscreen_mode = true
+config.audible_bell = "Disabled"
 
 config.bidi_enabled = true
 config.font = wezterm.font_with_fallback({ "Liga SFMono Nerd Font", "Vazir Code Hack" })
