0903120865a810afcfb9de70770ee6e8b08fb7bb
Abdomash <Abdomash2010@gmail.com>
2025-01-30 10:23:14 -0600

    Update colorscheme

--- lua/kickstart/plugins/color.lua
+++ lua/kickstart/plugins/color.lua
@@ -1,10 +1,10 @@
 return {
   {
-    'rebelot/kanagawa.nvim',
+    'loctvl842/monokai-pro.nvim',
     lazy = false,
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'kanagawa'
+      vim.cmd.colorscheme 'monokai-pro'
     end,
   },
 }
