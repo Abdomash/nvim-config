83b4595047b7ef242205188357c2106863d150df
Abdomash <Abdomash2010@gmail.com>
2025-02-18 17:42:40 -0600

    fix tmux-vim-navigation

--- lua/kickstart/plugins/tmux-navigator.lua
+++ lua/kickstart/plugins/tmux-navigator.lua
@@ -1,7 +1,13 @@
 return {
   -- Tmux & Neovim integration
   {
-    'christoomey/vim-tmux-navigator',
-    keys = { '<C-h>', '<C-j>', '<C-k>', '<C-l>' },
+    'alexghergh/nvim-tmux-navigation',
+    config = function()
+      local nvim_tmux_nav = require 'nvim-tmux-navigation'
+      vim.keymap.set('n', '<C-h>', nvim_tmux_nav.NvimTmuxNavigateLeft)
+      vim.keymap.set('n', '<C-j>', nvim_tmux_nav.NvimTmuxNavigateDown)
+      vim.keymap.set('n', '<C-k>', nvim_tmux_nav.NvimTmuxNavigateUp)
+      vim.keymap.set('n', '<C-l>', nvim_tmux_nav.NvimTmuxNavigateRight)
+    end,
   },
 }
